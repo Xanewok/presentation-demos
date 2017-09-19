03e299a78b799d479f3a388a51a7ce6638663472
Igor Matuszewski <Xanewok@gmail.com>
2017-09-19 19:09:19 +0200

    Add videos for IDE features

--- features/code_completion/src/util.rs
+++ features/code_completion/src/util.rs
@@ -1,8 +1,16 @@
+use std::fmt::Debug;
+
 /// A simple pair of `T` values.
 #[derive(Debug)]
 pub struct Pair<T> {
     pub x: T,
     pub y: T
+}
+
+impl<T> Pair<T> {
+    pub fn say_hi(&self) where T: Debug {
+        println!("{:?} says hi!", self);
+    }
 }
 
 /// Mix a `Pair<T>`, swapping its `x` and `y` values.
